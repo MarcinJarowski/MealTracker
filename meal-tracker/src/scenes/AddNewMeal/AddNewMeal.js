8b64054efe2e2eac4af38102b87bca386f8412f7
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2020-01-18 21:35:30 +0100

    Meal Search - dodano animacje rozwiniecia szukajki

--- meal-tracker/src/scenes/AddNewMeal/AddNewMeal.js
+++ meal-tracker/src/scenes/AddNewMeal/AddNewMeal.js
@@ -2,7 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import styles from "./AddNewMeal.module.css";
 import uuid from "uuid";
-import { MealCard } from "../../components/index";
+import { MealCard, Search } from "../../components/index";
 import {
   KeyboardBackspaceIcon,
   SearchIcon,
@@ -30,13 +30,18 @@ const AddNewMeal = props => {
     }
   }, [mealType]);
   // console.log(mealTypeName);
-  const handleArrowClick = event => {
+  const handleBackArrowClick = event => {
     return props.history.goBack();
   };
   const handleInputChange = value => {
     setInputValue(value);
   };
-
+  const handleSearchOpenStatus = () => {
+    setSearchOpenStatus(!isSearchOpenStatus);
+  };
+  const handleForwardArrowClick = () => {
+    setSearchOpenStatus(!isSearchOpenStatus);
+  };
   const {
     wrapper,
     topBar,
@@ -54,58 +59,53 @@ const AddNewMeal = props => {
     image,
     imageWrapper,
     searchMealName,
-    searchListWrapperExpanded
+    searchListWrapperExpanded,
+    boxShadow
   } = styles;
   //  wyjebać ustawianie klasy na open searcha to sie zrenderuje//
   return (
     <div className={wrapper}>
       <div className={topBar}>
-        <span className={goBack} onClick={handleArrowClick}>
+        <span className={goBack} onClick={handleBackArrowClick}>
           <KeyboardBackspaceIcon fontSize="large" />
         </span>
         <span className={mealTypeNameClass}>
           <p>Dodaj {mealTypeName}</p>
         </span>
       </div>
-      {/* <Search /> */}
       <div className={searchWrapper}>
-        <div className={search}>
+        <div className={isSearchOpenStatus ? `${search} ${boxShadow}` : search}>
           <input
             className={searchInput}
             value={inputValue}
             placeholder="Wyszukaj..."
             onChange={e => handleInputChange(e.target.value)}
+            onClick={() =>
+              !isSearchOpenStatus ? handleSearchOpenStatus() : null
+            }
           />
-          <span className={searchIconClass} onClick={setSearchOpenStatus(true)}>
+          <span
+            className={searchIconClass}
+            onClick={() => handleSearchOpenStatus()}
+          >
             <SearchIcon />
           </span>
         </div>
-        <div className={`${searchListWrapper} ${searchListWrapperExpanded}`}>
+        <div
+          className={
+            isSearchOpenStatus ? searchListWrapperExpanded : searchListWrapper
+          }
+        >
           <ul className={searchList}>
             <li className={searchListLi}>
               <span className={imageWrapper}>
                 <img class={image} src={mealPicPlaceholder} alt="meal" />
               </span>
               <p className={searchMealName}>Nazwa posiłku</p>
-              <span className={goBack}>
-                <ArrowRightAltIcon fontSize="large" />
-              </span>
-            </li>
-            <li className={searchListLi}>
-              <span className={imageWrapper}>
-                <img class={image} src={mealPicPlaceholder} alt="meal" />
-              </span>
-              <p className={searchMealName}>Nazwa posiłku</p>
-              <span className={goBack}>
-                <ArrowRightAltIcon fontSize="large" />
-              </span>
-            </li>
-            <li className={searchListLi}>
-              <span className={imageWrapper}>
-                <img class={image} src={mealPicPlaceholder} alt="meal" />
-              </span>
-              <p className={searchMealName}>Nazwa posiłku</p>
-              <span className={goBack}>
+              <span
+                className={goBack}
+                onClick={() => handleForwardArrowClick()}
+              >
                 <ArrowRightAltIcon fontSize="large" />
               </span>
             </li>
